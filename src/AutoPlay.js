16475a7d3ae6c6d29083ee1bbd349691cc06936a
tatecarson <tatecarson@gmail.com>
2020-04-11 22:07:47 -0500

    added dat.gui presets

--- src/AutoPlay.js
+++ src/AutoPlay.js
@@ -1,6 +1,9 @@
+import * as dat from 'dat.gui'
+import preset from './presets'
 import SimplexNoise from 'simplex-noise'
 import Srl from 'total-serialism/build/ts.es5.min.js'
 import Nexus from 'nexusui'
+
 const Mod = Srl.Transform
 const Rand = Srl.Stochastic
 
@@ -8,6 +11,46 @@ const simplex = new SimplexNoise(Math.random)
 
 const ID = 'autoPlay'
 
+const gui = new dat.GUI({
+  load: {
+    'preset': 'deeper',
+    'remembered': {
+      'Default': {
+        '0': {}
+      },
+      'preset1': {
+        '0': {
+          'pitch': 5.331566469093988,
+          'attack': 0.8720000000000002,
+          'release': 1.320000000000001,
+          'density': 0.6591024555461473
+        }
+      },
+      'deeper': {
+        '0': {
+          'pitch': 0.7274682472480949,
+          'attack': 0.05199999999999981,
+          'release': 1.320000000000001,
+          'density': 0.10635055038103303
+        }
+      }
+    },
+    'closed': false,
+    'folders': {}
+  }})
+
+const settings = {
+  attack: 0.1,
+  release: 0.1,
+  pitch: 1,
+  density: 1
+}
+gui.add(settings, 'pitch', 0.1, 6)
+gui.add(settings, 'attack')
+gui.add(settings, 'release')
+gui.add(settings, 'density', 0, 1)
+
+gui.remember(settings)
 export default class AutoPlay {
   constructor (granular) {
     this.granular = granular
@@ -44,19 +87,20 @@ export default class AutoPlay {
 
     const run = () => {
       x = simplex.noise2D(performance.now() / 10000, 0)
-      
+
       granular.set({
-        
+
         // pitch: dice.next(),
-        density: map(palindrome.next(), 0, 12, 0, 1), 
-        // density: 1,
+        pitch: settings.pitch,
+        // density: map(palindrome.next(), 0, 12, 0, 1),
+        density: settings.density,
         envelope: {
           // attack: map(x, -1, 1, 0, 0.5),
-          // attack: 0.1,
-          release: 0.1
+          attack: settings.release,
+          release: settings.attack
         }
       })
-      
+
       granular.updateVoice(ID, {
         position: map(palindrome.next(), 0, 12, 0, 1),
         volume: 0.5
@@ -80,4 +124,3 @@ export default class AutoPlay {
 function map (value, inMin, inMax, outMin, outMax) {
   return (value - inMin) * (outMax - outMin) / (inMax - inMin) + outMin
 }
-
---
+++ src/presets.js
@@ -0,0 +1,27 @@
+const preset = {
+  'preset': 'deeper',
+  'remembered': {
+    'Default': {
+      '0': {}
+    },
+    'preset1': {
+      '0': {
+        'pitch': 5.331566469093988,
+        'attack': 0.8720000000000002,
+        'release': 1.320000000000001,
+        'density': 0.6591024555461473
+      }
+    },
+    'deeper': {
+      '0': {
+        'pitch': 0.7274682472480949,
+        'attack': 0.05199999999999981,
+        'release': 1.320000000000001,
+        'density': 0.10635055038103303
+      }
+    }
+  },
+  'closed': false,
+  'folders': {}
+}
+export { preset }
